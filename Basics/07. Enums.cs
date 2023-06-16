ccc3d408f3b30c47be328df1071c8b8d3df6fbfa
laxman-learner <laxmandhami5459@gmail.com>
2023-06-16 15:54:51 +0545

    C # Enumerations

---
+++ Basics/07. Enums.cs
@@ -0,0 +1,23 @@
+using System;
+
+// enums are used to avoid hard coding which makes the code more readable and maintable
+enum DayOfWeek
+{
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+}
+
+internal class Demo
+{
+    public DayOfWeek day { get; set; }
+    void PrintGreeting()
+    {
+        if (day == DayOfWeek.Sunday)
+        {
+            Console.WriteLine("Greeting, Happay Sunday");
+        }
+        if (day == DayOfWeek.Saturday)
+        {
+            Console.WriteLine("Greeting, Happay Holiday");
+        }
+    }
+}
